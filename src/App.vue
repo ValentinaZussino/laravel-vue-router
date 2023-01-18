d7eb423f17c0a3eef14ec9bbb1bf9a4eca4c8ee7
ValentinaZussino <vzussino@protonmail.com>
2023-01-18 09:55:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Ciao</h1> 
+    <ul>
+      <li v-for="(project,i) in projects" :key="i">{{ project.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+  export default {
+    name: 'App',
+    data(){
+      return{
+        projects:[],
+        apiUrl: 'http://localhost:8000/api'
+      }
+    },
+  methods:{
+    getProjects(){
+      axios.get(`${this.apiUrl}/projects`).then((response)=>{
+        // console.log(response);
+        console.log(response.data.results.data);
+        this.projects = response.data.results.data;
+      }) 
+    }
+  },
+  mounted(){
+    this.getProjects()
+  }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
