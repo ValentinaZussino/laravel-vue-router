69cf4e515ae51f8347a637b9f036963de44c38a9
ValentinaZussino <vzussino@protonmail.com>
2023-01-19 15:52:55 +0100

    agg controlli frontend contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,20 +1,28 @@
 <template>
     <div>
+        <div v-if="success" class="alert alert-success text-start" role="alert">
+            Messaggio inviato con successo!
+        </div>
         <form class="col-12 text-start" @submit.prevent="sendForm()">
             <div class="mb-3">
-                <input class="border-0 border-bottom form-control" 
-                    type="text" name="name" id="name" placeholder="Name" v-model="name">
+                <input required class="border-0 border-bottom form-control" :class="{'is-invalid': errors.name}" type="text" name="name" id="name" placeholder="Name" v-model="name">
+                <p v-for="(error,index) in errors.name" :key="index" class="invalid-feedback">
+                    {{ error }}
+                </p>
             </div>
             <div class="mb-3">
-                <input class="border-0 border-bottom form-control" 
-                    type="text" name="email" id="email" placeholder="Email" v-model="email">
+                <input required class="border-0 border-bottom form-control" :class="{'is-invalid': errors.email}" type="text" name="email" id="email" placeholder="Email" v-model="email">
+                <p v-for="(error,index) in errors.email" :key="index" class="invalid-feedback">
+                    {{ error }}
+                </p>
             </div>
             <div class="mb-3">
-                <textarea class="border-0 border-bottom form-control"
-                    name="message" id="message" cols="30"
-                    rows="10" placeholder="Message" v-model="message"></textarea>
+                <textarea required class="border-0 border-bottom form-control" :class="{'is-invalid': errors.message}" name="message" id="message" cols="30" rows="10" placeholder="Message" v-model="message"></textarea>
+                <p v-for="(error,index) in errors.message" :key="index" class="invalid-feedback">
+                    {{ error }}
+                </p>
             </div>
-            <button class="btn btn-lg btn-primary text-white" type="submit" :disabled="loading">Send</button>
+            <button class="btn btn-lg btn-primary text-white" type="submit" :disabled="loading">{{loading ? 'Sending...' : 'Send'}}</button>
         </form>
     </div>
 </template>
@@ -32,11 +40,13 @@ import {store} from '../store';
                 email: '',
                 message: '',
                 success: false,
-                errors: {}
+                errors: {},
+                loading: false
             }
         },
         methods: {
             sendForm(){
+            this.loading = true;
                 const data = {
                     name: this.name,
                     email: this.email,
@@ -44,6 +54,15 @@ import {store} from '../store';
                 }
                 axios.post(`${this.store.apiBaseUrl}/contacts`, data).then((response)=>{
                     console.log(response.data);
+                    this.success = response.data.success;
+                    if(!this.success){
+                        this.errors = response.data.errors;
+                    }else{
+                        this.name = '',
+                        this.email = '';
+                        this.message = '';
+                    }
+                    this.loading = false;
                 })
             }
         }
