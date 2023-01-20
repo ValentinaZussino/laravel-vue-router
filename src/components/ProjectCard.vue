1bfa8716d13cedc47b2b6de76a4459aae9195a81
ValentinaZussino <vzussino@protonmail.com>
2023-01-20 16:14:52 +0100

    agg projectcard component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="card h-100" style="width: 18rem;">
+        <img v-if="project.cover_image" :src="`${store.imagBasePath}${project.cover_image}`" class="card-img-top" :alt="project.title">
+        <img v-else src="https://via.placeholder.com/1200x840/DDDDDD/444444?text=VZ+Portfolio" alt="C/O https://placeholder.com/">
+        <div class="card-body">
+            <h5 class="card-title">{{project.title}}</h5>
+            <div>
+                <p class="card-text">{{ truncateContent }}</p>
+            </div>
+            <router-link class="btn vz_btn_violet mt-2" :to="{name: 'single-project', params:{slug: project.slug}}">
+            Vedi il progetto
+            </router-link>                    
+        </div>
+    </div>
+</template>
+
+<script>
+import {store} from '../store';
+    export default {
+        name: 'ProjectCard',
+        props: ['project'],
+        data(){
+            return {
+                store,
+                contentMaxLen: 100
+            }
+        },
+        computed: {
+            truncateContent(){
+                if(this.project.description.length > this.contentMaxLen){
+                    return this.project.description.substr(0,this.contentMaxLen) + '...';
+                }
+                return this.project.description;
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -3,19 +3,7 @@
         <h1>Lista dei progetti</h1>
         <div class="row mb-5">
             <div class="col-12 col-md-4 mt-4" v-for="(project, i) in projects" :key="i">
-                <div class="card h-100" style="width: 18rem;">
-                    <img v-if="project.cover_image" :src="`${store.imagBasePath}${project.cover_image}`" class="card-img-top" :alt="project.title">
-                    <img v-else src="https://via.placeholder.com/1200x840/DDDDDD/444444?text=VZ+Portfolio" alt="C/O https://placeholder.com/">
-                    <div class="card-body">
-                        <h5 class="card-title">{{project.title}}</h5>
-                        <div>
-                            <p class="card-text">{{ truncateContent(project.description) }}</p>
-                        </div>
-                        <router-link class="btn vz_btn_violet mt-2" :to="{name: 'single-project', params:{slug: project.slug}}">
-                        Vedi il progetto
-                        </router-link>                    
-                    </div>
-                </div>
+                <ProjectCard :project="project"/>
             </div>
         </div>
         <nav aria-label="Page navigation example" class="">
@@ -30,41 +18,39 @@
 
 <script>
     import axios from 'axios';
+    import ProjectCard from '../components/ProjectCard.vue';
     import { store } from '../store';
     export default {
-        name: 'Projects',
-        data(){
-            return {
-                store,
-                projects: [],
-                currentPage: 1,
-                lastPage: null,
-                total: 0,
-                contentMaxLen: 100
-            }
-        },
-        methods:{
-            getProjects(pagenum){
-                axios.get(`${this.store.apiBaseUrl}/projects`,{params:{
-                   page: pagenum 
-                }}).then((response)=>{
-                   this.projects = response.data.results.data;
-                   this.currentPage = response.data.results.current_page;
-                   this.lastPage = response.data.results.last_page;
-                   this.total = response.data.results.total;
-                })
-            },
-           truncateContent(text){
-            if(text.length > this.contentMaxLen){
-                return text.substr(0,this.contentMaxLen) + '...';
-            }
-            return text;
-           },
-        },
-        mounted(){
-            this.getProjects(1);
+    name: "Projects",
+    components: {
+        ProjectCard
+    },
+    data() {
+        return {
+            store,
+            projects: [],
+            currentPage: 1,
+            lastPage: null,
+            total: 0,
+        };
+    },
+    methods: {
+        getProjects(pagenum) {
+            axios.get(`${this.store.apiBaseUrl}/projects`, { params: {
+                    page: pagenum
+                } }).then((response) => {
+                this.projects = response.data.results.data;
+                this.currentPage = response.data.results.current_page;
+                this.lastPage = response.data.results.last_page;
+                this.total = response.data.results.total;
+            });
         }
-    }
+    },
+    mounted() {
+        this.getProjects(1);
+    },
+    components: { ProjectCard }
+}
 </script>
 
 <style lang="scss" scoped>
